7b9d9fcb6cf0ecdbeb2a24c8e86c3390f33d2a7d
Gabor Szabo <gabor@szabgab.com>
2017-07-01 16:33:59 +0300

    watch application

--- server/wrap_todo.pl
+++ server/wrap_todo.pl
@@ -2,7 +2,7 @@ use v6;
 
 while True {
 	say "Starting";
-	shell('cd ~/work/Bailador-TODO/; BAILADOR=port:3002  perl6 -Ilib bin/app.pl'); 
+	shell('cd ~/work/Bailador-TODO/; BAILADOR=port:3002  bailador watch bin/app.pl6'); 
 	CATCH { default { put .^name, ': ', .Str } };
 }
 
