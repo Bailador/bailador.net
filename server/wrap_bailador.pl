36f3935d5a8aa61a32ca30f0c96394003907c459
Gabor Szabo <gabor@szabgab.com>
2017-07-01 17:28:34 +0300

    rename and watch

--- server/wrap_bailador.pl
+++ server/wrap_bailador.pl
@@ -2,7 +2,7 @@ use v6;
 
 while True {
 	say "Starting";
-	shell('cd ~/work/bailador.net/; BAILADOR=port:3001  perl6 -Ilib bin/app.pl'); 
+	shell('cd ~/work/bailador.net/; BAILADOR=port:3001 bailador watch bin/app.pl6'); 
 	CATCH { default { put .^name, ': ', .Str } };
 }
 
