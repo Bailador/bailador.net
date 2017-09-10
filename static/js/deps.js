9fb1201c31d8f95ebf98119d9d62d9b1553d5856
Emeric Fischer <fischer.emeric@gmail.com>
2017-09-10 16:34:01 +0200

    Remove prism.js

--- static/js/deps.js
+++ static/js/deps.js
@@ -1,13 +1,68 @@
+function get_travis(module) {
+    // TODO !!!
+    switch (module) {
+      case 'Digest':
+          return 'https://api.travis-ci.org/grondilu/libdigest-perl6.png';
+          break;
+      case 'Digest::HMAC':
+          return 'https://api.travis-ci.org/retupmoca/P6-Digest-HMAC.png';
+          break;
+      case 'File::Directory::Tree':
+          return;
+          break;
+      case 'File::Find':
+          return;
+          break;
+      case 'File::Temp':
+          return;
+          break;
+      case 'HTTP::Easy':
+          return;
+          break;
+      case 'HTTP::MultiPartParser':
+          return;
+          break;
+      case 'HTTP::Server::Ogre':
+          return;
+          break;
+      case 'HTTP::Status':
+          return;
+          break;
+      case 'Log::Any':
+          return;
+          break;
+      case 'Path::Iterator':
+          return;
+          break;
+      case 'Template::Mojo':
+          return;
+          break;
+      case 'Template::Mustache':
+          return;
+          break;
+      case 'Terminal::ANSIColor':
+          return;
+          break;
+      case 'URI':
+          return 'https://travis-ci.org/perl6-community-modules/uri.png';
+          break;
+      default:
+        return undefined;
+    }
+}
+
 var url = 'https://raw.githubusercontent.com/Bailador/Bailador/dev/META6.json';
 
 $.getJSON(url, function(json) {
   var items = []
   $.each(json.depends, function(k, v) {
-      items.push("<li>" + v + "</li>");
+      items.push("<li>" + v);
   });
 
   var list = document.getElementById("deps");
   for (var i = 0; i < items.length; i++) {
-    $("#deps").append(items[i]);
+    var module = items[i];
+    var travis = get_travis(module);
+    $("#deps").append(module + "<img alt='Travis Status' src='" + travis + "'/></li>");
   };
 });
